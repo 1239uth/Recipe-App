d8ca93476c74d5a90703b4222525e4518e1a1799
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2021-04-27 03:29:50 -0400

    fix: remove All from home screen

--- Recipe List App/Views/RecipeListView.swift
+++ Recipe List App/Views/RecipeListView.swift
@@ -34,7 +34,7 @@ struct RecipeListView: View {
                     
                 )
             }
-            .navigationBarTitle("All Recipes")
+            .navigationBarTitle("Recipes")
         }
     }
 }
