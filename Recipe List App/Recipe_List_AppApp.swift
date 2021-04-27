c4c68b650e89aee3e7656ad42980ba489e10556b
Uthman Mohamed <83053931+1239uth@users.noreply.github.com>
2021-04-26 23:46:05 -0400

    Initial Commit

---
+++ Recipe List App/Recipe_List_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipe_List_AppApp.swift
+//  Recipe List App
+//
+//  Created by Uthman Mohamed on 2021-04-26.
+//
+
+import SwiftUI
+
+@main
+struct Recipe_List_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
